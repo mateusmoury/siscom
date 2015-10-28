782f5d6c4d1ae3c7aca7e6475385777afe233dab
Tomer Simis <tls@cin.ufpe.br>
2015-10-28 08:31:53 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Estimators import lower_bound
 
 if __name__ == '__main__':
 
-	simulator = Simulator(initial_slots=10, min_tags=1, max_tags=1000, 
+	simulator = Simulator(initial_slots=10, simulations=10, min_tags=1, max_tags=1000, 
 							step=100, estimator=lower_bound)
 
 
