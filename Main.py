04b4a9b8300b5c7ba357a9186365bfd0b92011ea
Mateus <mmfrb@cin.ufpe.br>
2015-10-28 23:02:12 -0300

    mudando valores de parametros para ficar semelhante ao artigo

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Estimators import eom_lee
 
 if __name__ == '__main__':
 
-	simulator = Simulator(initial_slots=10, simulations=10, min_tags=1, max_tags=1000, 
+	simulator = Simulator(initial_slots=64, simulations=2000, min_tags=100, max_tags=1000,
 							step=100, estimator=eom_lee)
 
 
