f89f320f345ba25b08c93e99127ca807a3e44191
Mateus <mmfrb@cin.ufpe.br>
2015-11-09 17:16:37 -0300

    mudancas feitas na ultima aula

--- Estimators.py
+++ Estimators.py
@@ -19,3 +19,38 @@ def eom_lee(collisions, success, empty):
 			beta = new_beta
 
 	return int(gama * collisions)
+
+def chen(collisions, success, empty):
+	l = float(empty + success + collisions)
+	n = float(success + 2 * collisions)
+	next = 0.0
+	previous = - 1.0
+	while previous < next:
+		p_e = (1.0 -  (1.0 / l)) ** n
+		p_s = (n / l) * ((1.0 - (1.0 / l)) ** (n - 1.0))
+		p_c = 1.0 - p_e - p_s
+		previous = next
+		next = fat(l, empty, success, collisions) * (p_e ** empty) * (p_s ** success) * (p_c ** collisions)
+		n = n + 1.0
+	n = n - 2.0
+	return int(n)
+
+def fat(a,b,c,d):
+	res = 1.0
+	a = float(a)
+	b = float(b)
+	c = float(c)
+	d = float(d)
+	while a > 1:
+		res = res * a
+		a = a - 1.0
+		if b > 1:
+			res = res / b
+			b = b - 1.0
+		if c > 1:
+			res = res / c
+			c = c - 1.0
+		if d > 1:
+			res = res / d
+			d = d - 1.0
+	return res
--- Main.py
+++ Main.py
@@ -1,37 +1,78 @@
 from Simulator import Simulator
 from Estimators import *
 import matplotlib.pyplot as plt
+import time
 
 if __name__ == '__main__':
 
-	simulator = Simulator(initial_slots=64, simulations=1, min_tags=100, max_tags=1000,
+
+	lb = Simulator(initial_slots=64, simulations=500, min_tags=100, max_tags=1000,
+							step=100, estimator=lower_bound)
+
+	lee = Simulator(initial_slots=64, simulations=500, min_tags=100, max_tags=1000,
 							step=100, estimator=eom_lee)
+
+	chen = Simulator(initial_slots=64, simulations=500, min_tags=100, max_tags=1000,
+							step=100, estimator=chen)
 
 
 	# Use this to generate graphics
-	result = simulator.run()
+	start_time = time.time()
 
-	print(result)
+	lb_result = lb.run()
+	print("Tempo para execucao do lower bound: " + str(time.time() - start_time))
+
+	start_time = time.time()
+
+	lee_result = lee.run()
+	print("Tempo de execucao do eom lee: " + str(time.time() - start_time))
+
+	start_time = time.time()
+
+	chen_result = chen.run()
+	print("Tempo de execucao do chen: " + str(time.time() - start_time))
+
+	print(lb_result)
+	print(lee_result)
+	print(chen_result)
 	
 	# Generating Collisions vs Tags
 	plt.figure(1)
 	plt.title('Collisions vs Tags')
-	plt.plot([x['tags'] for x in result], [x['collisions'] for x in result], 'r--')
+	plt.plot([x['tags'] for x in lb_result], [x['collisions'] for x in lb_result], 'ro-', label="Lower Bound")
+	plt.plot([x['tags'] for x in lee_result], [x['collisions'] for x in lee_result], 'bo-', label="Eom Lee")
+	plt.plot([x['tags'] for x in chen_result], [x['collisions'] for x in chen_result], 'yo-', label="Chen")
 	plt.ylabel('Collisions')
 	plt.xlabel('Tags')
+	plt.legend()
 	
 	# Generating Empty vs Tags
 	plt.figure(2)
 	plt.title('Empty vs Tags')
-	plt.plot([x['tags'] for x in result], [x['empty'] for x in result], 'r--')
+	plt.plot([x['tags'] for x in lb_result], [x['empty'] for x in lb_result], 'ro-', label="Lower Bound")
+	plt.plot([x['tags'] for x in lee_result], [x['empty'] for x in lee_result], 'bo-', label="Eom Lee")
+	plt.plot([x['tags'] for x in chen_result], [x['empty'] for x in chen_result], 'yo-', label="Chen")
 	plt.ylabel('Empty')
 	plt.xlabel('Tags')
+	plt.legend()
 
 	# Generating Slots vs Tags
 	plt.figure(3)
 	plt.title('Slots vs Tags')
-	plt.plot([x['tags'] for x in result], [x['slots'] for x in result], 'r--')
+	plt.plot([x['tags'] for x in lb_result], [x['slots'] for x in lb_result], 'ro-', label="Lower Bound")
+	plt.plot([x['tags'] for x in lee_result], [x['slots'] for x in lee_result], 'bo-', label="Eom Lee")
+	plt.plot([x['tags'] for x in chen_result], [x['slots'] for x in chen_result], 'yo-', label="Chen")
 	plt.ylabel('Slots')
 	plt.xlabel('Tags')
+	plt.legend()
+
+	# Generating Slots vs Tags
+	# plt.figure(4)
+	# plt.title('Avg. abs. error vs Tags')
+	# plt.plot([x['tags'] for x in lb_result], [x['estimated'] for x in lb_result], 'ro-', label="Lower Bound")
+	# plt.plot([x['tags'] for x in lee_result], [x['estimated'] for x in lee_result], 'bo-', label="Eom Lee")
+	# plt.ylabel('Avg. Abs. Error')
+	# plt.xlabel('Tags')
+	# plt.legend()
 
 	plt.show()
