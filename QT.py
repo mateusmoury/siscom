269192b9372be99f80c762b0465fc583430bfecd
Marlon Reghert <mras@cin.ufpe.br>
2015-11-11 00:03:43 -0300

    Update QT.py

--- QT.py
+++ QT.py
@@ -8,6 +8,9 @@ class QT:
 	def __init__(self, tags):
 		self.tags = [{'tag': t, 'active': True} for t in tags]
 		self.tags_bits_sum = {} 
+		self.reader_bits_sum = 0 
+		self.steps = 0 
+		self.num_tags_to_identify = len(tags)
 		for t in tags:
 			self.tags_bits_sum[t] = 0
 
@@ -18,17 +21,21 @@ class QT:
 			'tag': "",
 		}
 		count = 0 
+		prefix_len = len(prefix)
 		for t in self.tags:
-			if t['active'] and t['tag'].startswith(prefix):
-				self.tags_bits_sum[t['tag']] += len(t['tag'])
-				result['tag'] = t['tag']
-				result['count']+=1
+			if t['active']:
+				self.reader_bits_sum += prefix_len
+				if t['tag'].startswith(prefix):
+					self.tags_bits_sum[t['tag']] += len(t['tag'])
+					result['tag'] = t['tag']
+					result['count']+=1
 		return result
 	
 	def execute(self, Q, M):
-		if Q.empty():
+		if Q.empty() or len(M) == self.num_tags_to_identify:
 			return M
 		prefix = Q.get()
+		self.steps+=1
 		queryResult = self.query(prefix)
 		if queryResult['count'] == 1:
 			tag = queryResult['tag']
@@ -44,18 +51,21 @@ class QT:
 	#	bits_sum: Sum of all bits exchanged, same as iterate over tags_results and sum all bits exchanged
 	#	bits_sum_average: bits_sum divided by the number of tags identified
 	def run(self):
-		results = {'tags_results':[], 'bits_sum': 0, 'bits_sum_average': 0.0 }	
+		results = { 'bits_sum': 0, 'bits_sum_average': 0.0 }	
 		Q = Queue()
 		Q.put("0")
 		Q.put("1")
 		M = []
 		M = self.execute(Q, [])
 		bits_sum = 0
+		
 		for tag in M:
 			bits_sum += self.tags_bits_sum[tag]
-			results['tags_results'].append({'tag': tag, 'bits_sum': self.tags_bits_sum[tag]})
+			#results['tags_results'].append({'tag': tag, 'bits_sum': self.tags_bits_sum[tag]})
 		results['bits_sum'] = bits_sum
-		results['bits_sum_average'] = float(bits_sum)/float(len(M))
+		results['bits_sum_average'] = float(bits_sum)/float(self.num_tags_to_identify)
+		results['reader_bits_sum'] = self.reader_bits_sum
+		results['steps'] = self.steps
 		return results
 
 
@@ -71,13 +81,11 @@ if __name__ == '__main__':
 			"100001",
 			"010110",
 			"011000",
-			"000100",
 			"010101",											
 			"100000",
 			"000010",
 			"000011",
 			"000001",
-			"000011",
 			"000101",
 			"000100",
 			"000110",
