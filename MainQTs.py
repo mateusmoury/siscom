6de1c9af0f77fd9db062d53043e4725186de7790
Mateus <mmfrb@cin.ufpe.br>
2015-11-10 23:18:45 -0300

    modificando MainQTs.py para ele funcionar

--- MainQTs.py
+++ MainQTs.py
@@ -3,10 +3,10 @@ from QT import QT
 #importar o QWT
 
 if __name__ == '__main__':
-	results_for_qt = {}
-	results_for_qwt = {}
+	results_for_qt = []
+	results_for_qwt = []
 
-	for number_of_tags in range(100, 10001, 100):
+	for number_of_tags in range(100, 1001, 100):
 		print("Rodando com numero de tags: " + str(number_of_tags))
 		qt_bits_sum = 0
 		qwt_bits_sum = 0
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 				qt_bits_sum += ans_qt['bits_sum']
 				# Fazer 18-20 para qwt
 		qt_bits_sum_mean = float(qt_bits_sum) / 1000.0
-		results_for_qt[number_of_tags] = qt_bits_sum_mean
+		results_for_qt.append([number_of_tags, qt_bits_sum_mean])
 		# Fazer 22-23 para qwt
 
 #Plotting
@@ -30,14 +30,14 @@ if __name__ == '__main__':
 	plt.figure()
 	plt.grid()
 	plt.title("#Tags vs Bits Transmitidos")
-	plt.plot([x[0] for x in results_for_qt.items()], [x[1] for x in results_for_qt.items()], 'ro-', label="QT")
+	plt.plot([x[0] for x in results_for_qt], [x[1] for x in results_for_qt], 'ro-', label="QT")
 	# Plotar qwt - semelhante linhas 33
 	# mas tem que mudar a cor (terceiro parametro)
 	# pode botar um 'bo-' e o label tambem!
 	plt.ylabel('Bits Transmitidos')
 	plt.xlabel('#Tags')
 	plt.legend()
-	plot.show()
+	plt.show()
 
 
 
